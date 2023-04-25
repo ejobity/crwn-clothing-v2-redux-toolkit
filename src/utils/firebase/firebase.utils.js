191b7930ca126ed4aebfa1850633f6c5ac5b67d1
Emile Jobity <ejobity@gmail.com>
2023-04-25 15:47:32 -0400

    Update firebase.utils.js

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -21,12 +21,12 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyBt-2rheMi4qWZqoHM7v1YgzIVNygMFGrw",
+  authDomain: "crwn-clothing-db-58656.firebaseapp.com",
+  projectId: "crwn-clothing-db-58656",
+  storageBucket: "crwn-clothing-db-58656.appspot.com",
+  messagingSenderId: "51919047285",
+  appId: "1:51919047285:web:ee8b25c6d51c039fe6063c"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
